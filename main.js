f3d7dbd618e123337781315274c1621a11b9560b
Sushil-Kharade <168211175+Sushil-Kharade@users.noreply.github.com>
2024-05-01 18:08:53 +0530

    about

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ const text = document.querySelector(".sec-text");
 
         const textLoad = () => {
             setTimeout(() => {
-                text.textContent = " Front-End  Developer";
+                text.textContent = " PF Agent";
             }, 0);
             setTimeout(() => {
-                text.textContent = " UI UX Designer";
+                text.textContent = " PF Agent";
             }, 4000);
            
         }
